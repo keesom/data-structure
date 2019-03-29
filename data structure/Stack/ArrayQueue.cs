fcb8a1dbcdc948e0623b9dd3b13071fe3e29d5a6
keesom <keesom@163.com>
2019-03-29 18:07:46 +0800

    队列

---
+++ data structure/Stack/ArrayQueue.cs
@@ -0,0 +1,49 @@
+﻿using Array;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStack
+{
+    public class ArrayQueue<T> : IQueue<T>
+    {
+        private Array<T> array;
+        public ArrayQueue(int capacity)
+        {
+            array = new Array<T>(8);
+        }
+        public ArrayQueue()
+        {
+            array = new Array<T>();//数组默认有10的容量
+        }
+        public T Dequeue()
+        {
+            return array.RemoveFirst();
+        }
+
+        public void Enqueue(T t)
+        {
+            array.AddLast(t);
+        }
+
+        public T GetFront()
+        {
+            return array.GetFirst();
+        }
+
+        public int GetSize()
+        {
+            return array.GetSize();
+        }
+
+        public bool IsEmpty()
+        {
+            return array.IsEmpty();
+        }
+        public int GetCapacity()
+        {
+            return array.GetCapacity();
+        }
+        
+    }
+}
---
+++ data structure/Stack/IQueue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStack
+{
+    public interface IQueue<T>
+    {
+
+        int GetSize();
+        bool IsEmpty();
+        //在队列中添加元素
+        void Enqueue(T t);
+        //从队列中拿出元素
+        T Dequeue();
+        T GetFront();
+    }
+}
